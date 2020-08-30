7d12562d9981bd8cfa2ab61d00d47a4d50ac60d8
GregBiglione <gregory.biglione81@gmail.com>
2020-08-30 19:06:57 +0200

    Added Facebook authentication on Firebase

--- app/src/main/java/com/greg/go4lunch/LoginRegisterActivity.java
+++ app/src/main/java/com/greg/go4lunch/LoginRegisterActivity.java
@@ -7,8 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -20,6 +27,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -36,6 +44,9 @@ public class LoginRegisterActivity extends AppCompatActivity {
     public static final int RC_SIGN_IN = 807;
     ProgressDialog mProgressDialog;
 
+    private CallbackManager mCallbackManager;
+    @BindView(R.id.facebook_login_button) LoginButton mFacebookLoginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,14 +55,17 @@ public class LoginRegisterActivity extends AppCompatActivity {
         fireBaseAuth();
         clickGoogleSignInButton();
         googleSignInConfigure();
-    }
 
-    // ---------------------------- GOOGLE AUTHENTICATION ------------------------------------------------------------------------------------------------------------
+        initFacebook();
+        initFacebookLoginButton();
+    }
 
     // ---------------------------- Initialize Firebase authentication -----------------------------
     private void fireBaseAuth(){
         mAuth = FirebaseAuth.getInstance();
     }
+
+    // ---------------------------- GOOGLE AUTHENTICATION ------------------------------------------------------------------------------------------------------------
 
     // ---------------------------- Configure Google Sign In ---------------------------------------
     public void googleSignInConfigure(){
@@ -94,9 +108,10 @@ public class LoginRegisterActivity extends AppCompatActivity {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
         }
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    // ---------------------------- Handle result  -------------------------------------------------
+    // ---------------------------- Handle Google result  ------------------------------------------
     private void handleSignInResult(Task<GoogleSignInAccount> task) {
         try {
             GoogleSignInAccount googleSignInAccount = task.getResult(ApiException.class);
@@ -130,7 +145,6 @@ public class LoginRegisterActivity extends AppCompatActivity {
 
     // ---------------------------- Get Google user information ------------------------------------
     private void updateUI(FirebaseUser user) {
-        //GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
         if (user != null){
             startActivity(new Intent(this, MainActivity.class));
         }
@@ -144,4 +158,60 @@ public class LoginRegisterActivity extends AppCompatActivity {
         updateUI(user);
     }
 
+    // ---------------------------- FACEBOOK AUTHENTICATION ----------------------------------------------------------------------------------------------------------
+
+    // ---------------------------- Initialize Facebook --------------------------------------------
+    private void initFacebook(){
+        mCallbackManager = CallbackManager.Factory.create();
+    }
+
+    // ---------------------------- Initialize Facebook login button -------------------------------
+    private void initFacebookLoginButton(){
+       mFacebookLoginButton = findViewById(R.id.facebook_login_button);
+       mFacebookLoginButton.setReadPermissions("email", "public_profile");
+       mFacebookLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+           @Override
+           public void onSuccess(LoginResult loginResult) {
+               Log.d(TAG, "facebook:onSuccess:" + loginResult);
+               Toasty.success(LoginRegisterActivity.this, "Login in with Facebook successfully", Toasty.LENGTH_SHORT).show();
+               handleFacebookAccessToken(loginResult.getAccessToken());
+           }
+
+           @Override
+           public void onCancel() {
+               Log.d(TAG, "facebook:onCancel:");
+               Toasty.warning(LoginRegisterActivity.this, "Login Facebook cancelled", Toasty.LENGTH_SHORT).show();
+           }
+
+           @Override
+           public void onError(FacebookException error) {
+               Log.d(TAG, "facebook:onError:" + error);
+               Toasty.error(LoginRegisterActivity.this, "Login Facebook error", Toasty.LENGTH_SHORT).show();
+           }
+       });
+    }
+
+    // ---------------------------- Handle Facebook result  ----------------------------------------
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+        Log.d(TAG, "handleFacebookAccessToken" + accessToken);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Log.d(TAG, "signInWithCredential:success");
+                            Toasty.success(LoginRegisterActivity.this, "Login Facebook successful", Toasty.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        }
+                        else{
+                            Log.w(TAG, "signInWithCredential:failure" + task.getException());
+                            Toasty.error(LoginRegisterActivity.this, "Login with Facebook failed", Toasty.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
 }

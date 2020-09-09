bd9417398fa040dcf43cb1e2491685155123b243
GregBiglione <gregory.biglione81@gmail.com>
2020-09-09 21:13:00 +0200

    Added workmates object

---
+++ app/src/main/java/com/greg/go4lunch/model/Workmates.java
@@ -0,0 +1,47 @@
+package com.greg.go4lunch.model;
+
+public class Workmates {
+    public String uid;
+    public String picture;
+    public String name;
+    public String email;
+
+    public Workmates(String uid, String picture, String name, String email) {
+        this.uid = uid;
+        this.picture = picture;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
